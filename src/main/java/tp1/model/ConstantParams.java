93a809b20a386dba3d1c51d67e7676346011550f
Dalencourt Alex <alex.dalencourt@yahoo.fr>
2018-01-15 17:34:16 +0100

    Mapping constantes

--- src/main/java/tp1/model/ConstantParams.java
+++ src/main/java/tp1/model/ConstantParams.java
@@ -1,9 +1,16 @@
 package tp1.model;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.Random;
+import java.util.Scanner;
 
 import tp1.sma.SMA;
+import tp1.sma.SMARandom;
 import tp1.sma.SMASequential;
+import tp1.sma.SMASequentialRandom;
 
 public class ConstantParams {
 	
@@ -12,54 +19,101 @@ public class ConstantParams {
 	private static Random random = null;
 	
 	private static SMA sma = null;
+
+	private static Properties props;
+	
+	static {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Path to properties file");
+		String path = scan.nextLine();
+		scan.close();
+		props = new Properties();
+		try {
+			props.load(new FileInputStream(new File(path)));
+		} catch (IOException e) {
+			throw new RuntimeException("Bad path to properties");
+		}
+	}
 	
 	public static int getNumberOfParticles() {
-		return 10;
+		return Integer.valueOf(props.getProperty("nb.particles"));
 	}
 	
 	public static Random getRandom() {
 		if(random == null) {
-			random = new Random(5);
+			int seed = Integer.valueOf(props.getProperty("seed"));
+			if(seed != 0) {
+				random = new Random(5);
+			}else {
+				random = new Random(System.currentTimeMillis());
+			}
 		}
 		return random;
 	}
 
 	public static int getGridSizeX() {
-		return 10;
+		return Integer.valueOf(props.getProperty("grid.size.x"));
 	}
 
 	public static int getGridSizeY() {
-		return 10;
+		return Integer.valueOf(props.getProperty("grid.size.y"));
 	}
 
 	public static boolean getTorus() {
-		return false;
+		return Boolean.getBoolean(props.getProperty("torus"));
 	}
 
 	public static SMA getSMA() {
 		if(sma == null) {
-			sma = new SMASequential();
+			switch (SMATypes.valueOf(props.getProperty("scheduling"))) {
+			case SEQUENTIAL:
+				sma = new SMASequential();
+				break;
+			case SEQUENTIALRANDOM:
+				sma = new SMASequentialRandom();
+				break;
+			case ALLRANDOM:
+			default:
+				sma = new SMARandom();
+				break;
+			}
 		}
 		return sma;
 	}
 
 	public static int getNumberOfTicks() {
-		return 10;
+		return Integer.valueOf(props.getProperty("nb.ticks"));
 	}
 
 	public static int getCanvasSizeX() {
-		return 500;
+		return Integer.valueOf(props.getProperty("canvas.size.x"));
 	}
 
 	public static int getCanvasSizeY() {
-		return 500;
+		return Integer.valueOf(props.getProperty("canvas.size.y"));
 	}
 
 	public static int getBoxSize() {
-		return 50;
+		return Integer.getInteger(props.getProperty("box.size"));
 	}
 
 	public static long getDelay() {
-		return 1000;
+		return Long.getLong(props.getProperty("delay"));
+	}
+	
+	public static boolean showGrid() {
+		return Boolean.getBoolean(props.getProperty("grid"));
+	}
+	
+	public static boolean showTrace() {
+		return Boolean.getBoolean(props.getProperty("trace"));
+	}
+	
+	public static int refresh() {
+		return Integer.getInteger(props.getProperty("refresh"));
+	}
+	
+	private enum SMATypes {
+		SEQUENTIAL, SEQUENTIALRANDOM, ALLRANDOM
 	}
 }
